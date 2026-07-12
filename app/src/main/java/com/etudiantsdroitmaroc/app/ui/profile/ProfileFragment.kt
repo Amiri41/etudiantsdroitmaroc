124c64b98f635da207960f5e9f6cc6c3f0e63e3c
Walid <walid@etudiantsdroitmaroc.app>
2026-07-12 19:16:58 +0000

    إصلاح: استيراد ناقص await فـ ProfileFragment

--- app/src/main/java/com/etudiantsdroitmaroc/app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/etudiantsdroitmaroc/app/ui/profile/ProfileFragment.kt
@@ -17,6 +17,7 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ProfileFragment : Fragment() {
 
